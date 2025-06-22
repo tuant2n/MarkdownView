b21ea3f53ee96192b6130b7b87aa08d36925aad2
Lakr <git@qaq.wiki>
2025-06-22 23:46:26 +0800

    Fix Issue on Right Click Without Selection

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-collections", from: "1.2.0"),
         .package(url: "https://github.com/mgriebling/SwiftMath", from: "1.7.1"),
         .package(url: "https://github.com/Lakr233/Splash", from: "0.17.0"),
-        .package(url: "https://github.com/Lakr233/Litext", from: "0.4.7"),
+        .package(url: "https://github.com/Lakr233/Litext", from: "0.4.8"),
         .package(url: "https://github.com/swiftlang/swift-cmark", from: "0.6.0"),
         .package(url: "https://github.com/nicklockwood/LRUCache", from: "1.0.7"),
     ],
