03f0546607a6305b76396fb6de90181dd1e7f13c
Lakr <git@qaq.wiki>
2025-07-06 03:06:54 +0800

    Revert "Fix code highlight queue order and simplify CodeView sizing"

--- Sources/MarkdownView/Components/CodeView/CodeView.swift
+++ Sources/MarkdownView/Components/CodeView/CodeView.swift
@@ -79,10 +79,17 @@ final class CodeView: UIView {
         let labelSize = languageLabel.intrinsicContentSize
         let barHeight = labelSize.height + CodeViewConfiguration.barPadding * 2
         let textSize = textView.intrinsicContentSize
+        let supposedHeight = Self.intrinsicHeight(for: content, theme: theme)
 
         return CGSize(
-            width: textSize.width + CodeViewConfiguration.codePadding * 2,
-            height: barHeight + textSize.height + CodeViewConfiguration.codePadding * 2
+            width: max(
+                labelSize.width + CodeViewConfiguration.barPadding * 2,
+                textSize.width + CodeViewConfiguration.codePadding * 2
+            ),
+            height: max(
+                barHeight + textSize.height + CodeViewConfiguration.codePadding * 2,
+                supposedHeight
+            )
         )
     }
 
