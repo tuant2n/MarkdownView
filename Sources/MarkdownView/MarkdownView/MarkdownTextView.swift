0d885115e890562e37e0b693a639834f099ddaa0
Lakr <git@qaq.wiki>
2025-07-06 00:15:31 +0800

    Update Litext

--- Sources/MarkdownView/MarkdownView/MarkdownTextView.swift
+++ Sources/MarkdownView/MarkdownView/MarkdownTextView.swift
@@ -286,21 +286,32 @@ extension MarkdownTextView {
     private func configureSubviews() {
         textView.backgroundColor = .clear
         textView.attributedText = attributedText ?? .init()
-        textView.tapHandler = { [weak self] highlightRegion, touchLocation in
-            guard let self else { return }
-            guard let highlightRegion else {
-                return
-            }
-            let link = highlightRegion.attributes[NSAttributedString.Key.link]
-            let range = highlightRegion.stringRange
-            if let url = link as? URL {
-                linkHandler?(.url(url), range, touchLocation)
-            } else if let string = link as? String {
-                linkHandler?(.string(string), range, touchLocation)
-            }
-        }
+        textView.delegate = self
         if textView.superview != self {
             addSubview(textView)
+        }
+    }
+}
+
+extension MarkdownTextView: LTXLabelDelegate {
+    public func ltxLabelSelectionDidChange(_: Litext.LTXLabel, selection _: NSRange?) {
+        // reserved for future use
+    }
+
+    public func ltxLabelDetectedUserEventMovingAtLocation(_: Litext.LTXLabel, location _: CGPoint) {
+        // reserved for future use
+    }
+
+    public func ltxLabelDidTapOnHighlightContent(_: LTXLabel, region: LTXHighlightRegion?, location: CGPoint) {
+        guard let highlightRegion = region else {
+            return
+        }
+        let link = highlightRegion.attributes[NSAttributedString.Key.link]
+        let range = highlightRegion.stringRange
+        if let url = link as? URL {
+            linkHandler?(.url(url), range, location)
+        } else if let string = link as? String {
+            linkHandler?(.string(string), range, location)
         }
     }
 }
